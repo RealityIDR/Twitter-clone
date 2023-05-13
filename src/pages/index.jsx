674bcbfc28b2ae6455b350adb197d61dd040f6b1
Isaac <realityidr.529@gmail.com>
2023-05-13 11:49:17 -0700

    Initialize Repository

---
+++ src/pages/index.jsx
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <Head>
+      <title>Twitter</title>
+      <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+    </Head>
+  )
+}
