a5874c2a645af81db0a6ba4230a11befceb1ba4f
Isaac <realityidr.529@gmail.com>
2023-05-15 14:12:26 -0700

    Progress update

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,22 @@
-import Head from 'next/head'
+import Feed from "@/components/Feed";
+import Sidebar from "@/components/Sidebar";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <Head>
-      <title>Twitter</title>
-      <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
-    </Head>
-  )
+    <div>
+      <Head>
+        <title>Twitter</title>
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+      </Head>
+
+      <main className="bg-black min-h-screen flex max-w-[1500px] mx-auto">
+        <Sidebar />
+        <Feed />
+        {/* Widgets */}
+
+        {/* Modal */}
+      </main>
+    </div>
+  );
 }
