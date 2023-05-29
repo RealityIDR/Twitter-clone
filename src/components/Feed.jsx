c2ca5587ebfd9db3290a7ac49431f03d882424d1
Isaac <realityidr.529@gmail.com>
2023-05-28 19:18:19 -0700

    Progress Update

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,25 @@
-import { SparklesIcon } from '@heroicons/react/24/outline'
-import React from 'react'
-import Input from './Input'
+import { useEffect, useState } from "react";
+import Input from "./Input";
+import { onSnapshot, collection, query, orderBy } from "@firebase/firestore";
+import { db } from "../../firebase";
+import { useSession } from "next-auth/react";
+import { SparklesIcon } from "@heroicons/react/24/outline";
+import Post from "./Post";
 
 function Feed() {
+  const [posts, setPosts] = useState([])
+
+  useEffect( 
+    () =>
+      onSnapshot(
+        query(collection(db, "posts"), orderBy("timestamp", "desc")),
+        (snapshot) => {
+          setPosts(snapshot.docs);
+        }
+      ),
+    [db]
+  );
+
   return (
     <div className='flex-grow text-white border-l border-r border-gray-700 max-w-2xl sm:ml-[73px] xl:ml-[370px]'>
         <div className='text-[#d9d9d9] flex items-center sm:justify-between py-2 px-3 sticky top-0 z-50 bg-black border-b border-gray-700'>
@@ -13,6 +30,12 @@ function Feed() {
         </div>
 
         <Input />
+
+        <div className='pb-72'>
+          {posts.map(post => (
+            <Post key={post.id} id={post.id} post={post.data()} />
+          ))}
+        </div>
 
     </div>
   )
