bd9d5ab875036dd555f61a3f2c5466b186cc5c54
Isaac <realityidr.529@gmail.com>
2023-08-12 14:32:28 -0700

    Bug Fixes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -18,10 +18,10 @@ function Sidebar() {
 
   return (
     <div className="hidden sm:flex flex-col items-center xl:items-start xl:w-[340px] p-2 fixed h-full">
-      <div className="flex items-center justify-center w-14 h-14 hoverAnimation p-0 xl:ml-24">
-        <Image src="/twitter-icon.png" width={30} height={30} />
+      <div className="flex items-center justify-center w-14 h-14 hoverAnimation p-0 xl:ml-20">
+        <Image src="/twitter-icon.png" alt="" width={30} height={30} />
       </div>
-      <div className="space-y-2.5 mt-4 mb-2.5 xl:ml-24">
+      <div className="space-y-2.5 mt-4 mb-2.5 xl:ml-20">
         <SidebarLink text="Home" Icon={HomeIcon} active />
         <SidebarLink text="Explore" Icon={HashtagIcon} />
         <SidebarLink text="Notifications" Icon={BellIcon} />
@@ -30,10 +30,10 @@ function Sidebar() {
         <SidebarLink text="Lists" Icon={ClipboardIcon} />
         <SidebarLink text="Profile" Icon={UserIcon} />
         <SidebarLink text="More" Icon={EllipsisHorizontalCircleIcon} />
-      </div>
-      <button className="hidden xl:inline ml-auto bg-[#1d9bf0] text-white rounded-full w-56 h-[52px] text-lg font-bold shadow-md hover:bg-[#1a8cd8]">
+        <button className="hidden xl:inline ml-auto bg-[#1d9bf0] text-white rounded-full w-full h-[52px] text-lg font-bold shadow-md hover:bg-[#1a8cd8]">
         Tweet
       </button>
+      </div>
       <div className="text-[#d9d9d9] flex items-center justify-center hoverAnimation xl:ml-auto xl:-mr-5 mt-auto" onClick={signOut}>
         <img
           src={session.user.image}
