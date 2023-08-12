9f1dc57b5dda6fc19a4b6609538947142b9e3d5a
Isaac <realityidr.529@gmail.com>
2023-08-12 14:32:59 -0700

    Added Guest Account functionality

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,6 +2,22 @@ import Image from "next/image";
 import { signIn } from "next-auth/react";
 
 function Login({ providers }) {
+
+  const handleSignIn = (e, providerId) => {
+    e.preventDefault()
+    if (providerId === 'credentials') {
+      signIn(providerId, {
+        email: 'john@gmail.com',
+        password: '1234',
+        redirect: false
+      })
+      
+    }
+    else {
+      signIn(providerId, { callbackUrl: "/" })
+    }
+  }
+
   return (
     <div className="flex flex-col items-center space-y-20 pt-48">
       <Image
@@ -9,18 +25,19 @@ function Login({ providers }) {
         width={150}
         height={150}
         style={{ objectFit: "contain" }}
+        alt=""
       />
       <div>
         {Object.values(providers).map((provider) => (
-          <div key={provider.name}>
+          <div className="mb-10 flex justify-center" key={provider.name}>
             {/* https://devdojo.com/tailwindcss/buttons#_ */}
             <button
               className="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-white rounded hover:bg-white group"
-              onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+              onClick={(e) => handleSignIn(e, provider.id)}
             >
               <span className="w-48 h-48 rounded rotate-[-40deg] bg-[#1d9bf0] absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
               <span className="relative w-full text-left text-black transition-colors duration-300 ease-in-out group-hover:text-white">
-                Sign in with {provider.name}
+                {provider.name === 'Credentials' ? 'Sign in using Test Account' : `Sign in with ${provider.name}`}
               </span>
             </button>
           </div>
--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -8,7 +9,19 @@ export const authOptions = {
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
-    // ...add more providers here
+    CredentialsProvider({
+      type: 'credentials',
+      credentials: {},
+      authorize(credentials, req){
+        const {email, password} = credentials
+
+        if (email !== "john@gmail.com" || password !== "1234") {
+          return null
+        }
+
+        return {id: "1234", name: "John Doe", email: 'john@gmail.com', image: 'https://firebasestorage.googleapis.com/v0/b/twitter-clone-5f2f5.appspot.com/o/Placeholder.png?alt=media&token=0eecc264-1226-4a07-a770-858ba11b4f86'}
+      }
+    })
   ],
   callbacks: {
     async session({ session, token }) {
